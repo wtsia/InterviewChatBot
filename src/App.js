3cd07c26dd793decf3a65ba051b3d33efe6603e7
Winston <winstontsia@gmail.com>
2022-12-14 04:47:35 -0800

    changed HTML

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
       <h1>Welcome to your Interview Chat.</h1>
       </header>
-      <h2>My name is Alice Bronwyn, head Fellow at PETA.<br />I did my JD at Georgetown Law.<br /> Today I will be conducting your interview.</h2>
+      <h2>My name is Alice Bronwyn, Head Fellow at PETA.<br />I did my JD at Georgetown Law.<br /> Today I will be conducting your interview.</h2>
         <Convo/>
       <footer>
         <p>For BB Interview, 2022</p>
